104646efc43d0ab840036cd9acd64c1c2404b0e5
Naveed Ahmad <naveedahmada036@gmail.com>
2017-07-01 09:49:55 +0500

    added translation for tv

--- lib/themoviedb/tv.rb
+++ lib/themoviedb/tv.rb
@@ -141,5 +141,11 @@ module Tmdb
       search = Tmdb::Search.new("/#{self.endpoints[:singular]}/#{self.endpoint_id + id.to_s}/alternative_titles")
       search.fetch_response
     end
+
+    # Get the translations for a specific series.
+    def self.translations(id, _conditions = {})
+      search = Tmdb::Search.new("/#{self.endpoints[:singular]}/#{self.endpoint_id + id.to_s}/translations")
+      search.fetch_response
+    end
   end
 end
